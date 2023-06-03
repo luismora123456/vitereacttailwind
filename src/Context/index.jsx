4279e91bd5c49bad0ae2cd09f484a7351bf86873
Luis Mora <lmora@maotsoft.com>
2023-06-02 23:04:17 -0600

    Open and close Product Detail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -5,8 +5,20 @@ export const ShoppingCartContext = createContext();
 // eslint-disable-next-line react/prop-types
 export const ShoppingCartProvider = ({ children }) => {
   const [count, setCount] = useState(0);
+  const [isProductDetailOpen, setIsProductDetailOpen] = useState(false);
+  const openProductDetail = () => setIsProductDetailOpen(true);
+  const closeProductDetail = () => setIsProductDetailOpen(false);
+
   return (
-    <ShoppingCartContext.Provider value={{ count, setCount }}>
+    <ShoppingCartContext.Provider
+      value={{
+        count,
+        setCount,
+        openProductDetail,
+        closeProductDetail,
+        isProductDetailOpen,
+      }}
+    >
       {children}
     </ShoppingCartContext.Provider>
   );
